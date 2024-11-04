7eae8cfa16702bdf46ed873ac1a02d823f1f4019
unknown <huhmlbb@gmail.com>
2024-11-04 15:12:10 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,54 @@
+//1
+// let arr = ["alpha","betta","gamma","delta","omega"]
+// console.log(arr[0]);
+// console.log(arr[arr.length-1]);
+
+//2
+// let number = [1,4,7,9,76,4,4,7,8,9,7,654,3,3,56,7]
+// let bigg = 0
+// for(i = 0; i < number.length; i++){
+//     if(number[i] > bigg){
+//         bigg = number[i]
+//     }
+// }
+// console.log(bigg);
+
+
+//3
+// let number = [1,4,7,9,76,4,4,7,8,9,7,654,3,3,56,7]
+// const a = 1
+// for(i = 0; i < number.length; i++){
+
+    // number[i]=number[i] + 1
+
+    // }
+    // console.log(number[i]);
+
+
+
+//4
+// let int = prompt("Son kiriting")-0
+// for (let i = 1; i <= int; i++) {
+//     if(int%i==0){
+//         console.log("Boluvchilari "+ i +" ga teng");
+//     }
+    
+// }
+
+
+
+//5
+// let number = [1,4,7,9,76,4,4,7,8,9,7,654,3,3,56,7]
+// let number2 = [6,64,77,99,76,43,4,73,83,96,7,6654,34,3,56,7]
+// let resArr = []
+
+// for(let i of number){
+//     for(let j of number2){
+//         if(i == j){
+//             resArr.push(i)
+//         }
+//     }
+// }
+// console.log(resArr);
+
+
